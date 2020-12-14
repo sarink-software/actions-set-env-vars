83104e110ddcfb55a407c0509c73941f7887efff
Kabir Sarin <sarink87@gmail.com>
2020-12-13 23:01:46 -0600

    Handle files not existing

--- index.ts
+++ index.ts
@@ -7,8 +7,13 @@ import path from 'path';
 try {
   type Env = 'prod' | 'qa' | 'stage' | 'dev';
 
-  const nvmrc = fs.readFileSync('.nvmrc', 'utf-8');
-  if (nvmrc) core.setOutput('NODE_VERSION', nvmrc);
+  const nvmrcExists = fs.existsSync('.nvmrc');
+  if (nvmrcExists) {
+    const nvmrc = fs.readFileSync('.nvmrc', 'utf-8');
+    core.setOutput('NODE_VERSION', nvmrc);
+  } else {
+    core.info('No .nvmrc file found, skipping setting NODE_VERSION output.');
+  }
 
   const { eventName, ref } = github.context;
   const refToEnv: Record<string, Env> = {
@@ -21,18 +26,26 @@ try {
     develop: 'dev',
   };
   const env = eventName === 'release' ? 'prod' : refToEnv[ref];
-  if (!env) throw new Error(`Could not find a suitable env for github.ref: ${ref}`);
+  const envExists = env && fs.existsSync(`.env.${env}`);
+  if (envExists) {
+    core.info(`Copying .env.${env} --> .env`);
+    fs.copyFileSync(`.env.${env}`, '.env');
 
-  core.debug(`Copying .env.${env} --> .env`);
-  fs.copyFileSync(`.env.${env}`, '.env');
+    const envFile = path.resolve(__dirname, '.env');
+    const dotEnvConfig = dotenv.config({ path: envFile });
+    const parsedEnvFile = dotEnvConfig.parsed;
+    if (dotEnvConfig.error || !parsedEnvFile) throw dotEnvConfig.error;
+    Object.entries(parsedEnvFile).forEach(([key, val]) => {
+      core.info(`Setting output: ${key}`);
+      core.setOutput(key, val);
+    });
+  } else {
+    core.info(`Could not find a .env file for env: ${env}`);
+  }
 
-  const envFile = path.resolve(__dirname, '.env');
-  const dotEnvConfig = dotenv.config({ path: envFile });
-  const parsedEnvFile = dotEnvConfig.parsed;
-  if (dotEnvConfig.error || !parsedEnvFile) throw dotEnvConfig.error;
-  Object.entries(parsedEnvFile).forEach(([key, val]) => {
-    core.setOutput(key, val);
-  });
+  if (!envExists && !nvmrcExists) {
+    throw new Error('No .nvmrc or .env file found');
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
